7081f839085732bcb0c1815c4235cb5562274b46
sw4 <samuel.wheeler@gmail.com>
2014-02-26 11:14:02 +0000

    Update sliderBar.js

--- sliderBar.js
+++ sliderBar.js
@@ -97,7 +97,8 @@ $.fn.extend({
                 var animate=sliderBar.attr('data-sliderBar-animate');
                 var progressBar=sliderBar.children('.sliderBar-progress');
                 if(animate!=='false'){
-                    progressBar.animate({(vertical ? 'height' : 'width'), Math.floor(100 * val / max) + '%'}, animate);
+                    var property=vertical ? 'height' : 'width';
+                    progressBar.animate({property, Math.floor(100 * val / max) + '%'}, animate);
                 }else{
                     progressBar.css(vertical ? 'height' : 'width', Math.floor(100 * val / max) + '%');
                 }
